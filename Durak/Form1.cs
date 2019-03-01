44914071b26c9e7280cd3944e585e52bb84a82ac
Steven Hitchon <steven.hitchon@dcmail.ca>
2019-03-01 11:58:11 -0500

    added a card class to work on and comments, every will need to add their name

---
+++ Durak/Form1.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * Durak Game created for OOP 4200 2019
+ * 
+ * @Authors: Steven Hitchon
+ * Created: March 1 2019
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Durak
+{
+    public partial class frmDurakGame : Form
+    {
+        public frmDurakGame()
+        {
+            InitializeComponent();
+        }
+    }
+}
